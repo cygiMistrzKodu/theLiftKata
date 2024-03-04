d25e294bde2eea49fa3551416d97711f9d895049
Jacek programmer <jacek301@gmail.com>
2024-03-04 23:26:00 +0100

    refactor get rid of unnecessary part

--- src/main/java/Dinglemouse.java
+++ src/main/java/Dinglemouse.java
@@ -10,26 +10,19 @@ public class Dinglemouse {
 
         }
 
-        for (int florIndex = 1; florIndex < queues.length; florIndex++) {
-
-            if (queues.length > 2 && queues[florIndex].length > 0) {
-
-                int floorToGo = queues[florIndex][0];
-                if ( floorToGo == 0 ) {
-                    return new int[]{0, florIndex, 0};
-                }
-
-            }
-
-        }
-
 
         for (int floorIndex = 1; floorIndex <= queues.length; floorIndex++) {
 
             if (queues.length > 2 && queues[floorIndex].length > 0) {
 
                 int floorToGo = queues[floorIndex][0];
+
+                if (floorToGo == 0) {
+                    return new int[]{0, floorIndex, 0};
+                }
+
                 return new int[]{0, floorIndex, floorToGo, 0};
+
 
             }
 
