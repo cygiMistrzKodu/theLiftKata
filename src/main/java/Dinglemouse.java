21a2dedb4d21713e1a6ad48bdcec545beb09f4c3
Jacek programmer <jacek301@gmail.com>
2024-03-24 21:53:53 +0100

    Return to previous implementation

--- src/main/java/Dinglemouse.java
+++ src/main/java/Dinglemouse.java
@@ -7,16 +7,11 @@ public class Dinglemouse {
 
         List<Integer> liftStops = new ArrayList<>();
 
-        Map<Integer, List<Integer>> nextPeopleWillBeTaken = new HashMap<>();
-
-
         if (queues[0].length == 0) {
             liftStops.add(0);
         }
 
         List<Integer> floorsPeopleWantToGo = new ArrayList<>();
-
-        // czy trzeba wracać na piętra ?? bo ludzie czekaja a nie było miejsca ??
 
         for (int floor = 0; floor < queues.length; floor++) {
 
@@ -39,52 +34,10 @@ public class Dinglemouse {
 
             }
 
-            if (capacity >= queues[floor].length) {
-
-                List<Integer> peopleIndexOnCurrentFloorTaken = new ArrayList<>();
-                peopleIndexOnCurrentFloorTaken.add(0, 0);
-                peopleIndexOnCurrentFloorTaken.add(1, queues[floor].length);
-
-                nextPeopleWillBeTaken.put(floor, peopleIndexOnCurrentFloorTaken);
+            for (int peopleIndex = 0; peopleIndex < queues[floor].length; peopleIndex++) {
+                floorsPeopleWantToGo.add(queues[floor][peopleIndex]);
             }
 
-//            if (capacity < queues[floor].length) {  // moze cos innego
-//
-//                if (nextPeopleWillBeTaken.containsKey(floor)) {
-//
-//                    List<Integer> peopleIndexOnCurrentFloor = nextPeopleWillBeTaken.get(floor);
-//
-//                    Integer previouslyFinishStart = peopleIndexOnCurrentFloor.get(0);
-//
-//                    Integer nextPeopleStartIndex = previouslyFinishStart + capacity;
-//
-//                    Integer previouslyFinishStopIndex = peopleIndexOnCurrentFloor.get(1);
-//
-//                    Integer nextPeopleFinishStopIndex = previouslyFinishStopIndex + capacity;
-//
-//                    List<Integer> peopleIndexOnCurrentFloorTaken = new ArrayList<>();
-//                    peopleIndexOnCurrentFloorTaken.set(0, nextPeopleStartIndex);
-//                    peopleIndexOnCurrentFloorTaken.set(1, nextPeopleFinishStopIndex);
-//
-//                    nextPeopleWillBeTaken.put(floor, peopleIndexOnCurrentFloorTaken);
-//
-//                } else {
-//
-//                    List<Integer> peopleIndexOnCurrentFloorTaken = new ArrayList<>();
-//                    peopleIndexOnCurrentFloorTaken.add(0, 0);
-//                    peopleIndexOnCurrentFloorTaken.add(1, capacity);
-//                    nextPeopleWillBeTaken.put(floor, peopleIndexOnCurrentFloorTaken);
-//                }
-//
-//            }
-
-            int[] peopleWaitingForLiftOnFloor = queues[floor];  // moze jakos wstawic 0 dlatego ze musi winda wrocic a potem kolejnego brac
-            List<Integer> startAndStopIndexDependingOnFloor = nextPeopleWillBeTaken.get(floor);
-
-            int[] peopleGotToLift = Arrays.stream(peopleWaitingForLiftOnFloor,
-                    startAndStopIndexDependingOnFloor.get(0), startAndStopIndexDependingOnFloor.get(1)).toArray();
-
-            floorsPeopleWantToGo.addAll(Arrays.stream(peopleGotToLift).boxed().toList());
 
             if (floorsPeopleWantToGo.contains(floor)) {
 
